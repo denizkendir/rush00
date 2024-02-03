481b143874c701362a3a99206866c088e2ce96fd
denizkendir <116739330+denizkendir@users.noreply.github.com>
2024-02-03 13:44:47 +0300

    Add files via upload

---
+++ rush04/ex00/rush03.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush03.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dkendir <dkendir@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/28 04:05:51 by eakyurek          #+#    #+#             */
+/*   Updated: 2024/02/03 13:43:39 by dkendir          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	ft_print(int len, char being, char mid, char end)
+{
+	int	letter_counter;
+
+	letter_counter = 1;
+	while (letter_counter <= len)
+	{
+		if (letter_counter == 1)
+			ft_putchar(being);
+		else if (letter_counter == len)
+			ft_putchar(end);
+		else
+			ft_putchar(mid);
+		letter_counter++;
+	}
+	ft_putchar('\n');
+}
+
+void	rush(int x, int y)
+{
+	int	line_counter;
+
+	line_counter = 1;
+	if (x >= 1 && y >= 1)
+	{
+		while (line_counter <= y)
+		{
+			if (line_counter == 1)
+				ft_print(x, 'A', 'B', 'C');
+			else if (line_counter == y)
+				ft_print(x, 'A', 'B', 'C');
+			else
+				ft_print(x, 'B', ' ', 'B');
+			line_counter++;
+		}
+	}
+	if (x <= 0 || y <= 0)
+	{
+		write(1, "Hata! X ve Y değerleri 0 ve 0'dan küçük olamaz!", 52);
+	}
+}
